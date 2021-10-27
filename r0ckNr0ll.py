f43cf3da0d8a2f594b5de80064bbff590bfbf368
R0GUEXE <87347528+R0GUEXE@users.noreply.github.com>
2021-10-27 16:59:48 -0400

    Update r0ckNr0ll.py

--- r0ckNr0ll.py
+++ r0ckNr0ll.py
@@ -6,20 +6,20 @@ from pyfiglet import Figlet
 
 fnt = Figlet(font="thin")
 print (fnt.renderText("r0ck'N'r0ll"))
-time.sleep(3)
+time.sleep(dlay)
 
 txt = input("What would you like to spam? ")
 
+dlay = input("How long do you want the delay before the spam to be? "
 
 tmes = input("How many times do you want it to spam?: ")
-
 
 
 rdy = input("Are you ready to r0ck'N'r0ll? (y/n, default=n): ")
 
 if rdy == ("y"):
     print("r0ckin'N'r0lling!")
-    print("***(Make sure your cursor is in the right text input. YOU HAVE 10 SECONDS!)***")
+    print(f"***(Make sure your cursor is in the right text input. YOU HAVE {dlay} SECONDS!)***")
 
 else:
     print("Quitting... Come back When you're ready!")
