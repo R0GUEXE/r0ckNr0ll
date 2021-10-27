ec3790c78ac37c594d772b18e8efd33ba0af37c3
R0GUEXE <87347528+R0GUEXE@users.noreply.github.com>
2021-10-27 19:30:57 -0400

    Update r0ckNr0ll.py

--- r0ckNr0ll.py
+++ r0ckNr0ll.py
@@ -1,32 +1,39 @@
-#r0ckNr0ll.py
+# r0ckNr0ll.py
 
 import pyautogui
 import time
 from pyfiglet import Figlet
 
 fnt = Figlet(font="thin")
-print (fnt.renderText("r0ck'N'r0ll"))
+print(fnt.renderText("r0ck'N'r0ll"))
 time.sleep(5)
 
-txt = input("What would you like to spam? ")
 
-dlay = input("How long do you want the delay before the spam to be? "
+def r0ck0n():
+    txt = input("What would you like to spam?: ")
 
-tmes = input("How many times do you want it to spam?: ")
+    dlay = input("How long do you want the delay before the spam to be?: ")
+
+    tmes = input("How many times do you want it to spam?: ")
+
+    rdy = input("Are you ready to r0ck'N'r0ll? (y/m/q): ")
+
+    if rdy == "y":
+        print("r0ckin'N'r0lling!")
+        print(f"***(Make sure your cursor is in the right text input. YOU HAVE {dlay} SECONDS!)***")
+
+    if rdy == "m":
+        r0ck0n()
+
+    if rdy == "q":
+        print("Quitting!")
+        time.sleep(3)
+        quit()
+
+    time.sleep(int(dlay))
+    for i in range(int(tmes)):
+        pyautogui.typewrite(txt)
+        pyautogui.press("enter")
 
 
-rdy = input("Are you ready to r0ck'N'r0ll? (y/n, default=n): ")
-
-if rdy == ("y"):
-    print("r0ckin'N'r0lling!")
-    print(f"***(Make sure your cursor is in the right text input. YOU HAVE {dlay} SECONDS!)***")
-
-else:
-    print("Quitting... Come back When you're ready!")
-    time.sleep(3)
-    quit()
-
-time.sleep(dlay)
-for i in range(int(tmes)):
-    pyautogui.typewrite(txt)
-    pyautogui.press("enter")
+r0ck0n()
